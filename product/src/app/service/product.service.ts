4497cec492ced412eeef4a723b6e4524da864889
Rishabh Gist <damyrachit@gmail.com>
2022-11-19 04:32:19 +0530

    added methods in service class

--- product/src/app/service/product.service.ts
+++ product/src/app/service/product.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http:) { }
+  constructor(private http: HttpClient) { }
+  URL: string = 'http://localhost:8080/api/v1/addItem'
+  
+  post(item: Product) {
+    return this.http.post(this.URL, item);
+  }
+  getAll() {
+    return this.http.get<Array<Product>>(this.URL);
+  }
 }
