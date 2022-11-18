5ba6b5e3bdd37d2603d74d154c8fd54af7429448
Rishabh Gist <damyrachit@gmail.com>
2022-11-19 01:56:34 +0530

    set home as always display

--- product/src/app/app-routing.module.ts
+++ product/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -6,7 +6,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
   },
   {
