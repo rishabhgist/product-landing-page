ab860e0692628e4cac746920f562f4fc56842e6f
Rishabh Gist <damyrachit@gmail.com>
2022-11-19 03:23:51 +0530

    added product test data

--- product/src/app/home/home.component.ts
+++ product/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+  products: Product[] = [];
 }
