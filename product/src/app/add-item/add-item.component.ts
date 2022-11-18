cffb434c401256425566fde0a39df30da87c9785
Rishabh Gist <damyrachit@gmail.com>
2022-11-19 04:25:03 +0530

    added method to save data

--- product/src/app/add-item/add-item.component.ts
+++ product/src/app/add-item/add-item.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-add-item',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-item.component.scss']
 })
 export class AddItemComponent {
+  constructor(private fb: FormBuilder) { }
+  item = this.fb.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required],
+    category: ['', Validators.required]
+  })
 
+  addItem(item: any) {
+    console.log(item.value);
+    
+  }
 }
