e14ab47a6708ebc525583f8bca8596dbeb19ffee
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:11:59 +0530

    added abstract method to add product

--- server/src/main/java/com/jap/product/service/IProductService.java
+++ server/src/main/java/com/jap/product/service/IProductService.java
@@ -1,4 +1,13 @@
 package com.jap.product.service;
 
+import com.jap.product.domain.Product;
+
 public interface IProductService {
+    /**
+     * Add a product to the database
+     *
+     * @param product The product object to be added.
+     * @return The product that was added.
+     */
+    Product add(Product product);
 }
