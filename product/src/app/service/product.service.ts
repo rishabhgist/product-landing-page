969f62774ecea436c04b029fbc47c9b95ed85db9
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:45:55 +0530

    fixed getAll return type

--- product/src/app/service/product.service.ts
+++ product/src/app/service/product.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from '../model/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  constructor(private http: HttpClient) {}
+  URL: string = 'http://localhost:8080/api/v1/addItem';
 
-  constructor(private http: HttpClient) { }
-  URL: string = 'http://localhost:8080/api/v1/addItem'
-  
   post(item: any) {
     return this.http.post(this.URL, item);
   }
-  getAll() {
+  getAll(): Observable<Array<Product>> {
     return this.http.get<Array<Product>>(this.URL);
   }
 }
