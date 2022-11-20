5da7f643d3ba0bf6d814688a49f9dad2296a9263
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:20:40 +0530

    added autowired annotation for ProductService constructor

--- server/src/main/java/com/jap/product/service/ProductService.java
+++ server/src/main/java/com/jap/product/service/ProductService.java
@@ -2,6 +2,7 @@ package com.jap.product.service;
 
 import com.jap.product.domain.Product;
 import com.jap.product.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class ProductService implements IProductService {
 
     private ProductRepository productRepository;
 
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
