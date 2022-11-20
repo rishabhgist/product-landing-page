16b2a3f3ec149fed2f5e88a91ebc29edf790dc99
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:08:26 +0530

    added getters and setters in Product Class

--- server/src/main/java/com/jap/product/domain/Product.java
+++ server/src/main/java/com/jap/product/domain/Product.java
@@ -15,4 +15,36 @@ public class Product {
 
     public Product() {
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
