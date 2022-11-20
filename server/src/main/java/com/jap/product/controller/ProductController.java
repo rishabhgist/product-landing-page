0e5535943e70f759aef3a5531030c730ba88f558
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:28:20 +0530

    added RestController annotation in ProductController class

--- server/src/main/java/com/jap/product/controller/ProductController.java
+++ server/src/main/java/com/jap/product/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.jap.product.controller;
 
 import com.jap.product.service.ProductService;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProductController {
     private ProductService productService;
 
