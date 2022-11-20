15246a352d73a1abc2021adcf1618f6c2ce19c26
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:34:50 +0530

    added RequestMapping to Initialize api

--- server/src/main/java/com/jap/product/controller/ProductController.java
+++ server/src/main/java/com/jap/product/controller/ProductController.java
@@ -5,10 +5,10 @@ import com.jap.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
     private ProductService productService;
 
