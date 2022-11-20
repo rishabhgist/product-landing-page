edabcc6301a3fd655f20c66d026354cdf9e99079
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:14:18 +0530

    added ProductService implement class

--- server/src/main/java/com/jap/product/service/ProductService.java
+++ server/src/main/java/com/jap/product/service/ProductService.java
@@ -1,5 +1,29 @@
 package com.jap.product.service;
 
-public class ProductService {
+import com.jap.product.domain.Product;
 
+import java.util.List;
+
+public class ProductService implements IProductService {
+
+    /**
+     * Add a product to the database
+     *
+     * @param product The product object to be added.
+     * @return The product that was added.
+     */
+    @Override
+    public Product add(Product product) {
+        return null;
+    }
+
+    /**
+     * This function returns a list of products.
+     *
+     * @return A list of products.
+     */
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
 }
