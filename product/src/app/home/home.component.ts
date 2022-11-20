3dec5369bcfd469fdd93344b3f143b71ffa748aa
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:47:32 +0530

    injected ProductService in contrstuctor

--- product/src/app/home/home.component.ts
+++ product/src/app/home/home.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../model/product';
+import { ProductService } from '../service/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private productService: ProductService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   products: Product[] = [];
+
+ 
 }
