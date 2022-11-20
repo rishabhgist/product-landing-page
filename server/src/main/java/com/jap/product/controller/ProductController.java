542bd7abf943d1a3ee4eb30c5a17c55bc23e8862
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:29:33 +0530

    added autowired to connect ProductService object with ProductRepository

--- server/src/main/java/com/jap/product/controller/ProductController.java
+++ server/src/main/java/com/jap/product/controller/ProductController.java
@@ -1,12 +1,14 @@
 package com.jap.product.controller;
 
 import com.jap.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
     private ProductService productService;
 
+    @Autowired
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
