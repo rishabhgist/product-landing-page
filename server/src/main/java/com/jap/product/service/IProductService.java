3720dfd0a51ca098b828ed9a674479e0efcf8b5d
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:12:57 +0530

    added abstract method to fetch all product

--- server/src/main/java/com/jap/product/service/IProductService.java
+++ server/src/main/java/com/jap/product/service/IProductService.java
@@ -2,6 +2,8 @@ package com.jap.product.service;
 
 import com.jap.product.domain.Product;
 
+import java.util.List;
+
 public interface IProductService {
     /**
      * Add a product to the database
@@ -10,4 +12,11 @@ public interface IProductService {
      * @return The product that was added.
      */
     Product add(Product product);
+
+    /**
+     * This function returns a list of products.
+     *
+     * @return A list of products.
+     */
+    List<Product> getAll();
 }
