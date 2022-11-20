305286ca933a9a7e7b4fc153eea5cac29bad464c
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:50:32 +0530

    added CrossOrigin annotation remove API restrictions

--- server/src/main/java/com/jap/product/controller/ProductController.java
+++ server/src/main/java/com/jap/product/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1")
 public class ProductController {
