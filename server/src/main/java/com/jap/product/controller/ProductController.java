40d9af116a786f4dc4e23a6d83d24769ffda579c
Rishabh Gist <damyrachit@gmail.com>
2022-11-21 00:33:05 +0530

    added GetMapping with GetAll method

--- server/src/main/java/com/jap/product/controller/ProductController.java
+++ server/src/main/java/com/jap/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.jap.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,5 +20,10 @@ public class ProductController {
     @PostMapping("/post")
     public ResponseEntity<?> save(@RequestBody Product product) {
         return new ResponseEntity<>(productService.add(product), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<?> getAll() {
+        return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 }
